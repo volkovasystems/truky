2395c1733b1187774e471c11d926fed28c9d463d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-05 10:18:29 +0800

    Updated server/client test.

--- truky-test.js
+++ truky-test.js
@@ -2,7 +2,7 @@
 const assert = require( "assert" );
 const truky = require( "./truky.js" );
 
-assert.equal( Array.isArray( truky( { } ) ), true, "should return true" );
+assert.equal( truky( { } ).length == 0, true, "should return true" );
 
 assert.deepEqual( truky( { "name": "simple" } ), [ "simple" ], "should be deeply equal" );
 
