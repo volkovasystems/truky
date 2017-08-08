79b7633e3e624c89b80006621f85648836c733d7
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 16:13:42 +0800

    Updated test.

--- truky-test.js
+++ truky-test.js
@@ -2,10 +2,10 @@
 const assert = require( "assert" );
 const truky = require( "./truky.js" );
 
-assert.equal( truky( { } ).length == 0, true, "should return true" );
+assert.equal( truky( { } ).length == 0, true, "should be equal to true" );
 
-assert.deepEqual( truky( { "name": "simple" } ), [ "simple" ], "should be deeply equal" );
+assert.deepEqual( truky( { "name": "simple" } ), [ "simple" ], "should be equal to [ 'simple' ]" );
 
-assert.deepEqual( truky( function Hello( ){ } ), [ "Hello" ], "should be deeply equal" );
+assert.deepEqual( truky( function Hello( ){ } ), [ "Hello" ], "should be equal to [ 'Hello' ]" );
 
 console.log( "ok" );
